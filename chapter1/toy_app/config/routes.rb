693ef7d4c8dcc2892fe073780cd636aeefce39c2
Joan Gu <Huiqiong.Gu@walmartlabs.com>
2017-11-11 20:52:59 -0800

    adding toy_app

--- chapter1/toy_app/config/routes.rb
+++ chapter1/toy_app/config/routes.rb
@@ -1,9 +1,14 @@
-HelloApp::Application.routes.draw do
+Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#goodbye'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
